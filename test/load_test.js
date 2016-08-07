1a2b858d88bd6e2aaebc219f585e23db353a5d82
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-07 21:41:25 +0900

    Support dynamic load

--- test/load_test.js
+++ test/load_test.js
@@ -20,7 +20,23 @@ describe('load', function () {
   }))
 
   it('Load', () => co(function * () {
-
+    let module = load({
+      hey () {
+        const s = this
+        return s._hey
+      },
+      $before () {
+        const s = this
+        s._hey = (s._hey || 0) + 1
+      },
+      $after () {
+        const s = this
+        s._hey = (s._hey || 0) * 2
+      }
+    })
+    assert.equal(yield module.hey(), 1)
+    assert.equal(yield module.hey(), 3)
+    assert.equal(yield module.hey(), 7)
   }))
 })
 
