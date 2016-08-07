1852d61d89f943c7c81c212c33caf0731ace0621
Taka Okunishi <okunishitaka.com@gmail.com>
2016-08-07 21:39:34 +0900

    Add load method

---
+++ load.js
@@ -0,0 +1,4 @@
+'use strict'
+
+// Alias for `require('rfunc-client/load')
+module.exports = require('./lib/load')
---
+++ test/load_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for load.
+ * Runs with mocha.
+ */
+'use strict'
+
+const load = require('../lib/load.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('load', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Load', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
